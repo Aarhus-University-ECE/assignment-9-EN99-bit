f273a270c33c968674812fb4eb8c6c64d791d02f
EN99-bit <112312714+EN99-bit@users.noreply.github.com>
2022-11-07 23:29:32 +0100

    Final Draft

--- src/dfs.c
+++ src/dfs.c
@@ -7,16 +7,38 @@
 #include <stdbool.h> /* bool, true, false */
 #include "dfs.h"
 
-void DFT(node *root)
+void DFT(node *root) // Implement DFS
 {
-  // Implement DFS
-   // Allocating memory for node
+  stack *new_stack = malloc(sizeof(stack)); // Creating new stack
+  push(new_stack, root);                    // Calling push to place the root into the stack
+  new_stack->next = NULL;                   // new->next is at NULL from the beginning
+
+  while (!(isEmpty)(new_stack)) // loop until stack is empty
+  {
+    stack *q = pop(new_stack);
+    if (q->node->rchild != NULL)
+    {
+      push(new_stack, q->node->rchild); // call push function with the new stack and right child as arguments
+    }
+    if (q->node->lchild)
+    {
+      push(new_stack, q->node->lchild); // same with left child. Push to stack when found
+    }
+  }
+
+  // step 1: Allocate memory for node
+  // step 2: Check if l-child and r-child are pointing at nothing to see if we have reached the depth of the tree
   // Hint: You can use print_node, print_tree and/or print_stack.
 }
 
 node *make_node(int num, node *left, node *right)
 {
-  return 0;
+  node *new_node = malloc(sizeof(node)); // node depends on leftchild, rightchild, number, and if we have been there before
+  new_node->lchild = left;
+  new_node->num = num;
+  new_node->rchild = right;
+  new_node->visited = false;
+  return new_node;
 }
 
 void free_node(node *p)
@@ -54,17 +76,24 @@ void print_tree(node *p, int depth)
 
 stack *push(stack *topp, node *node)
 {
+  stack *element = malloc(sizeof(stack));
+  element->next = topp->next;
+  element->node = node;
+  topp->next = element;
   return 0;
 }
 
-bool isEmpty(stack *topp)
+bool isEmpty(stack *topp) // if stack is empty
 {
-  return false;
+  if (topp->next = NULL)
+    return true;
+  else
+    return false;
 }
 
 node *top(stack *topp)
 {
-  return 0;
+  return topp->node;
 }
 
 // Utility function to pop topp
@@ -72,7 +101,9 @@ node *top(stack *topp)
 
 stack *pop(stack *topp)
 {
-  return 0;
+  stack *Popped_element = 0;
+  Popped_element = topp->next;
+  topp->next = topp->next->next;
 }
 
 void print_stack(stack *topp)
--- src/sum.c
+++ src/sum.c
@@ -5,10 +5,10 @@ int sum(int a[], int n) //sum function
     if (n == 0)
     {
         return 0; // if n == 0, return  0
-                  // if n>0 return the sum
+                  
     }
 
-    if (n > 0)
+    if (n > 0)  // if n>0 return the sum
     {
         return sum(a, n - 1) + a[n - 1];
     }
