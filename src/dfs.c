53364354b7893f3a728df54d93a9769e65ed985b
EN99-bit <112312714+EN99-bit@users.noreply.github.com>
2022-12-01 16:14:27 +0100

    DFS done

--- src/dfs.c
+++ src/dfs.c
@@ -6,26 +6,30 @@
 #include <stdlib.h>  /* abort */
 #include <stdbool.h> /* bool, true, false */
 #include "dfs.h"
+#include <assert.h>
 
 void DFT(node *root) // Implement DFS
 {
   stack *new_stack = malloc(sizeof(stack)); // Creating new stack
-  push(new_stack, root);                    // Calling push to place the root into the stack
-  new_stack->next = NULL;                   // new->next is at NULL from the beginning
+  new_stack = push(new_stack, root);                    // Calling push to place the root into the stack
 
-  while (!(isEmpty)(new_stack)) // loop until stack is empty
+  while (!(isEmpty(new_stack))) // loop until stack is empty
   {
     stack *q = pop(new_stack);
+
+    printf("%d\n",q->node->num);
+
+    assert(q != NULL);
+
     if (q->node->rchild != NULL)
     {
-      push(new_stack, q->node->rchild); // call push function with the new stack and right child as arguments
+      new_stack = push(new_stack, q->node->rchild); // call push function with the new stack and right child as arguments
     }
-    if (q->node->lchild)
+    if (q->node->lchild != NULL)
     {
-      push(new_stack, q->node->lchild); // same with left child. Push to stack when found
+      new_stack = push(new_stack, q->node->lchild); // same with left child. Push to stack when found
     }
   }
-
   // step 1: Allocate memory for node
   // step 2: Check if l-child and r-child are pointing at nothing to see if we have reached the depth of the tree
   // Hint: You can use print_node, print_tree and/or print_stack.
@@ -34,8 +38,8 @@ void DFT(node *root) // Implement DFS
 node *make_node(int num, node *left, node *right)
 {
   node *new_node = malloc(sizeof(node)); // node depends on leftchild, rightchild, number, and if we have been there before
-  new_node->lchild = left;
   new_node->num = num;
+  new_node->lchild = left;
   new_node->rchild = right;
   new_node->visited = false;
   return new_node;
@@ -77,18 +81,23 @@ void print_tree(node *p, int depth)
 stack *push(stack *topp, node *node)
 {
   stack *element = malloc(sizeof(stack));
-  element->next = topp->next;
+  // element->next = topp->next;
   element->node = node;
-  topp->next = element;
-  return 0;
+  element->next = topp;
+  return element;
 }
 
 bool isEmpty(stack *topp) // if stack is empty
 {
-  if (topp->next = NULL)
+  if (topp->next == NULL)
+  {
+    printf("\nStack is empty\n");
     return true;
+  }
   else
+  {
     return false;
+  }
 }
 
 node *top(stack *topp)
@@ -102,8 +111,10 @@ node *top(stack *topp)
 stack *pop(stack *topp)
 {
   stack *Popped_element = 0;
-  Popped_element = topp->next;
-  topp->next = topp->next->next;
+  Popped_element = topp;
+  topp = topp->next;
+  printf("\n%d\n", Popped_element->node->num);
+  return topp;
 }
 
 void print_stack(stack *topp)
